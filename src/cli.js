c423969dde4896323cd77b54d866f91f45b0a4e5
Hasan Ali <hasan15.06.96@gmail.com>
2025-01-28 21:29:20 +0000

    cli tool

---
+++ src/cli.js
@@ -0,0 +1,25 @@
+import fs from "fs-extra";
+import path from "path";
+import process from "process";
+
+export async function cli(args) {
+  let scaffoldingPath;
+
+  if (args.database) {
+    scaffoldingPath = path.resolve(
+      new URL(import.meta.url).pathname,
+      "../../scaffolding"
+    );
+  } else {
+    scaffoldingPath = path.resolve(
+      new URL(import.meta.url).pathname,
+      "../../scaffolding/next"
+    );
+  }
+
+  const destinationPath = process.cwd() + "/" + args.projectName;
+
+  fs.mkdirSync(destinationPath);
+
+  await fs.copy(scaffoldingPath, destinationPath);
+}
